b277f35868d1b61159018c860318ecef41ae50d7
axelpro04 <axellefpro@gmail.com>
2021-04-30 09:47:52 +0200

    exo

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function touch() {
+    var lastName = document.getElementById('lastName').value;
+    var firstName = document.getElementById('firstName').value;
+    var city = document.getElementById('city').value;
+    alert('Nom : ' + lastName + '\n' + 'Prénom : ' + firstName + '\n' + 'Ville : ' + city);
+}
